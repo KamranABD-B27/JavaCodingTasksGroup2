d302d03c66c727651684fb19a1bb7f66cafcf0d4
Mayira <mahira223@hotmail.com>
2022-09-22 09:48:34 -0400

    oddOrEven class updated

--- src/week1/oddOrEven.java
+++ src/week1/oddOrEven.java
@@ -1,4 +1,21 @@
-package week1;
+package src.week1;
 
 public class oddOrEven {
+    /*
+    Numbers -- odd & even
+       Write a method which can identifies given number is even or odd
+     */
+    public static void oddOrEven(int number){
+
+        if (number % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        oddOrEven(5);
+    }
 }
