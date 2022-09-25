23ffb9852a0dc69e5dcda49d1f04ef40a4d6fe67
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-09-25 17:06:03 -0400

    OddOrEven1 and OddOrEven2 options added and updated

---
+++ src/week1/oddOrEven1.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class oddOrEven1 {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 6;
+
+                if(num1 % 2 != 0){
+                    System.out.println(num1 + " is odd");
+                }
+
+                if(num2 % 2 == 0){
+                    System.out.println(num2 + " is even");
+
+        }
+    }
+}
---
+++ src/week1/oddOrEven2.java
@@ -0,0 +1,25 @@
+package week1;
+
+import java.util.Arrays;
+
+public class oddOrEven2 {
+    public static void main(String[] args) {
+
+        int[] nums = {5, 6};
+        int even = 0, odd = 0;
+
+        for(int each: nums){
+            if(each % 2 == 0){
+                even++; {
+                    System.out.println(each + " is even");
+                }
+            } else {
+                odd++;
+                {
+                    System.out.println(each + " is odd");
+                }
+            }
+        }
+
+    }
+}
