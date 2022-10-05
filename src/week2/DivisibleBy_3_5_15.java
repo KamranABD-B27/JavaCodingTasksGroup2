244d782c870972c097e18c78769bc3c64050c632
Yasemin Parlakyigit <yasminpyigit@gmail.com>
2022-10-05 10:44:32 -0400

    DivisibleBy_3_5_15

--- src/week2/DivisibleBy_3_5_15.java
+++ src/week2/DivisibleBy_3_5_15.java
@@ -2,7 +2,36 @@ package week2;
 
 public class DivisibleBy_3_5_15 {
 
+    public static void main(String[] args) {
+
+        ///divisible by3,5,15
+
+        String divisibleby3 = "";
+        String divisibleby5 = "";
+        String divisibleby15 = "";
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                divisibleby15 += i + " ";
+
+            } else if (i % 3 == 0) {
+                divisibleby3 += i + " ";
+
+            } else if (i % 5 == 0) {
+                divisibleby5 += i+" ";
+
+            }
 
 
-    
+        }
+
+        System.out.println("divisibleby3 = " + divisibleby3);
+        System.out.println("divisibleby5 = " + divisibleby5);
+        System.out.println("divisibleby15 = " + divisibleby15);
+
+
+    }
 }
+
+
+
