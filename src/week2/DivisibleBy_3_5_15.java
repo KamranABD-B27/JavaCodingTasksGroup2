38cfa7f2c0e8db4141817e1277323853f8c841f5
Yasemin Parlakyigit <yasminpyigit@gmail.com>
2022-10-05 16:22:17 -0400

    PrintConsecutiveNumbers

--- src/week2/DivisibleBy_3_5_15.java
+++ src/week2/DivisibleBy_3_5_15.java
@@ -6,28 +6,28 @@ public class DivisibleBy_3_5_15 {
 
         ///divisible by3,5,15
 
-        String divisibleby3 = "";
-        String divisibleby5 = "";
         String divisibleby15 = "";
+        String divisibleby5 = "";
+        String divisibleby3 = "";
 
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
                 divisibleby15 += i + " ";
 
-            } else if (i % 3 == 0) {
-                divisibleby3 += i + " ";
-
             } else if (i % 5 == 0) {
-                divisibleby5 += i+" ";
+                divisibleby5 += i + " ";
+
+            } else if (i % 3 == 0) {
+                divisibleby3 += i+" ";
 
             }
 
 
         }
 
-        System.out.println("divisibleby3 = " + divisibleby3);
+        System.out.println("divisibleby3 = " + divisibleby15);
         System.out.println("divisibleby5 = " + divisibleby5);
-        System.out.println("divisibleby15 = " + divisibleby15);
+        System.out.println("divisibleby15 = " + divisibleby3);
 
 
     }
