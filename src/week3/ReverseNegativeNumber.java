67899fac619e660e18f54d6731199bbc514e9605
Anar Ismayilov <anarissoy@gmail.com>
2022-10-06 08:16:49 -0400

    ReverseNegativeNumber.java update

--- src/week3/ReverseNegativeNumber.java
+++ src/week3/ReverseNegativeNumber.java
@@ -1,4 +1,9 @@
 package week3;
 
 public class ReverseNegativeNumber {
+    /*
+    3. Number -- Reverse negative number
+Write a return method that can reverse
+negative number and return it as int
+     */
 }
