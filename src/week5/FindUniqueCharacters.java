80535b86a6e39dd6c5c9d3637641dc9af8654aa1
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-10-19 14:33:13 -0400

    week5 package with 3 tasks (empty java classes) created and pushed

---
+++ src/week5/FindUniqueCharacters.java
@@ -0,0 +1,7 @@
+package week5;
+//Write a return method that can find the unique characters from the String
+// Ex: unique("AAABBBCCCDEF") ==> "DEF"
+
+public class FindUniqueCharacters {
+
+}
---
+++ src/week5/Reverse.java
@@ -0,0 +1,7 @@
+package week5;
+//Write a return method that can reverse String
+//Ex: Reverse("ABCD"); ==> DCBA
+
+public class Reverse {
+
+}
