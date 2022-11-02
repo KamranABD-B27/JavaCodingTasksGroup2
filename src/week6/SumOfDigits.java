e47e88c9240b854a9330149083eaaa6dc283f23d
Mayira <mahira223@hotmail.com>
2022-11-02 19:19:18 -0400

    SumOfDigits updated

--- src/week6/SumOfDigits.java
+++ src/week6/SumOfDigits.java
@@ -4,4 +4,20 @@ Write a method that can return the sum of the digits in a string
  */
 
 public class SumOfDigits {
+    
+    public static int sumOfDigits(String str){
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if (Character.isDigit(str.charAt(i))){
+                sum += Integer.parseInt(str.charAt(i)+"");  // "9" -> 9
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String str = "eo1g9;fj23nf?s";
+        System.out.println(sumOfDigits(str));
+    }
 }
