6dbac32caaf616e94b9abf2e20b7777780e4c97a
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-11-13 21:26:13 -0500

    Sort Descending Task Solution

--- src/week7/SortDescending.java
+++ src/week7/SortDescending.java
@@ -6,7 +6,25 @@ Ex: int[] arr = {10,20,7, 8, 90};
 arr = Sort(arr); ==> {90, 20, 10, 8, 7};
  */
 
+import java.util.Arrays;
+
 public class SortDescending {
 
+    public static int[] sortingArrayDescending(int[] arr) {
+        int[] resultDescending = Arrays.copyOfRange(arr, 0, arr.length);
+        for (int i = 0; i < resultDescending.length; i++) {
+            for (int j = 0; j < resultDescending.length; j++) {
+                if (resultDescending[i] > resultDescending[j]) {
+                    Integer temp = resultDescending[i];
+                    resultDescending[i] = resultDescending[j];
+                    resultDescending[j] = temp;
+                }
+            }
+        }
+        return resultDescending;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 10, -8, -1, 3, -2, 13};
+        System.out.println(Arrays.toString(sortingArrayDescending(arr)));
+    }
 }
-
