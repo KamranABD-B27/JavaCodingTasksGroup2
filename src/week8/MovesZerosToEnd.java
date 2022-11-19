1db34b1bfce5cda601af48347ae65b557b9e3dd4
Mayira <mahira223@hotmail.com>
2022-11-18 23:20:36 -0500

    MovesZerosToEnd added

--- src/week8/MovesZerosToEnd.java
+++ src/week8/MovesZerosToEnd.java
@@ -7,6 +7,38 @@ input: {1,0,2,0,3,0,4,0};
 output: [1, 2, 3, 4, 0, 0, 0, 0]
  */
 
+import java.util.Arrays;
+
 public class MovesZerosToEnd {
+
+    public static int[] moveZerosToTheEnd(int[] nums){
+
+        int[] result = new int[nums.length];  //
+        int count = 0;
+
+        for (int each: nums){
+            if (each != 0){
+                result[count++] = each;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,0,2,0,3,0,4,0};
+
+        int[] result = new int[nums.length];  //
+        int count = 0;
+
+        for (int each: nums){
+            if (each != 0){
+                result[count++] = each;
+            }
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
 
 }
