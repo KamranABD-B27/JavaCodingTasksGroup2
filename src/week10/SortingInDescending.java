803efad75f164b1f1e137179c27a6b872583bb87
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-11-23 17:34:32 -0500

    The week 10 package with 3 empty files created and pushed

---
+++ src/week10/SortingInDescending.java
@@ -0,0 +1,9 @@
+package week10;
+/*
+ArrayList - sorting in descending order
+Write a method that can sort the ArrayList in descending order without using the sort method
+ */
+
+public class SortingInDescending {
+
+}
---
+++ src/week10/SortingMapByValues.java
@@ -0,0 +1,9 @@
+package week10;
+/*
+Map -- Sort the map by values
+Write a method that can sort the Map by values
+ */
+
+public class SortingMapByValues {
+
+}
