7b27154b19ebe2d16fc483bf61cbb52e5930a7d5
Denys Vorobiov <denis200920092009@gmail.com>
2022-11-23 15:08:31 -0500

    Password Validation task, solution added

--- src/week9/RemoveValues.java
+++ src/week9/RemoveValues.java
@@ -4,6 +4,20 @@ ArrayList - Remove some values
 Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
  */
 
-public class RemoveValues {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class RemoveValues {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,222,333,454,6,7,8,101));
+        for (int i = 0; i < list1.size(); i++) {
+//            list1.removeIf(n -> n > 100);  //ready made method
+            if (list1.get(i) > 100){
+                list1.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list1);
+    }
 }
+
