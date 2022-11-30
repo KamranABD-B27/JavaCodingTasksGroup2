2eda4d4799397fe2b1887b68f718bd962d47a005
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-11-30 17:37:52 -0500

    The week 10 java task solutions done

--- src/week10/SortingInDescending.java
+++ src/week10/SortingInDescending.java
@@ -4,6 +4,27 @@ ArrayList - sorting in descending order
 Write a method that can sort the ArrayList in descending order without using the sort method
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SortingInDescending {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5,55,4,44));
+        SortingArrayListAsc(list);
+    }
+    public static void SortingArrayListAsc(List<Integer> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
+                    Integer temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        System.out.println(list);
+    }
 
 }
