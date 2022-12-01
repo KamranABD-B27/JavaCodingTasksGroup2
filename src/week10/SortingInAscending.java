6850efe84833d85c975a3e7e9615ff47e9c7776c
marjan <marzhansuluu@gmail.com>
2022-12-01 07:57:08 -0700

    sorting ArrayList asc order

--- src/week10/SortingInAscending.java
+++ src/week10/SortingInAscending.java
@@ -4,6 +4,36 @@ ArrayList - sorting in ascending
 Write a method that can sort the ArrayList in Ascending order without using the sort method
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SortingInAscending {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(0, 4, 10, -2));
+
+        arrayList.add(-10);
+        arrayList.add(15);
+        arrayList.add(5);
+
+        System.out.println("Before sorting = " + arrayList);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+
+            for (int j = arrayList.size() - 1; j > i; j--) {
+                if (arrayList.get(i) > arrayList.get(j)) {// this inner loop will compare every single arraylist
+                    //element with i value.
+
+                    int temp = arrayList.get(i);//I gave arrayList's i value for the temp variable
+                    arrayList.set(i, arrayList.get(j));//I used set() method in order to replace the i with j.
+                    arrayList.set(j, temp); // I used set() again to replace the j with temp.
+
+                }
+
+            }
+
+        }
+        System.out.println("After sorting = " + arrayList);
+
+    }
 
 }
