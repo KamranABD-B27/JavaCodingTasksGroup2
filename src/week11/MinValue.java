5127b737d28eedb5a0e36d915248a7eb3103821a
Mayira <mahira223@hotmail.com>
2022-12-07 15:26:24 -0500

    MinValue updated

--- src/week11/MinValue.java
+++ src/week11/MinValue.java
@@ -5,5 +5,35 @@ Map -- Min value
 Write a method that can return the minimum value from ta map (DO NOT use sort method)
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinValue {
+
+    public static Integer minValue(Map<String,Integer> map){
+
+        Integer minValue = Integer.MAX_VALUE;
+
+        for (String eachKey: map.keySet()){
+
+            if (map.get(eachKey) < minValue){
+                minValue = map.get(eachKey);
+            }
+        }
+
+        return minValue;
+
+    }
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> map = new HashMap<>();
+        map.put("apple", 4);
+        map.put("banana",1);
+        map.put("pear", 5);
+        map.put("kiwi", 9);
+
+        System.out.println(minValue(map));
+
+    }
 }
