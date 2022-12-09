6a5e082c805ad52601d36d27c7d890958522f3c8
Guliru Fulati <gulrupolat04@gmail.com>
2022-12-08 20:48:41 -0500

    Gulru's commit

--- src/week11/UniqueCharacters.java
+++ src/week11/UniqueCharacters.java
@@ -8,5 +8,27 @@ For example:
 {b=1,e=1,t=1}
  */
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class UniqueCharacters {
+    public static void UniqueChar(String str){
+        Map<Character,Integer> map = new LinkedHashMap<>();
+        for(char each: str.toCharArray()){
+            if(map.containsKey(each)){
+                map.remove(each);
+
+                }else{
+                map.put(each,1);
+
+
+            }
+        }
+        System.out.println(map);
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabddbschhhy";
+        UniqueChar(str);
+    }
 }
